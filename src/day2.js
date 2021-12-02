f4c1f09dbdb59e54e6ad064fc1ec8e05f2f51c7f
Roger Guasch <roger.guasch.rion@gmail.com>
2021-12-02 15:33:06 +0100

    day2

---
+++ src/day2.js
@@ -0,0 +1,18 @@
+const letter = "bici coche balón _playstation bici coche peluche";
+
+export default function listGifts(letter) {
+  return letter
+    .split(" ")
+    .filter((word) => word !== "" && word.startsWith("_") !== true)
+    .reduce((acc, word) => {
+      if (word in acc) {
+        acc[word]++;
+      } else {
+        acc[word] = 1;
+      }
+      return acc;
+    }, {});
+}
+
+const numberOfGifts = listGifts(letter);
+console.log(numberOfGifts);
